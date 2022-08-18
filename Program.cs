bb2437a4509d0fff64778f2de770d4ddbc2f6e91
EmmanuelLio05 <70165675+EmmanuelLio05@users.noreply.github.com>
2022-08-17 22:56:59 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiparadigma {
+    class Program {
+        static void Main(string[] args) {
+            Program IrA = new Program();
+            List<int> nNumeros = new List<int>(){1,4,7,25,267,3,45,673,34,68,35654,387,543,22354};
+
+            Console.WriteLine("---------------------------");
+            IrA.DentroDeRango(new Rango(30, 80), nNumeros).ForEach(Console.WriteLine);
+            Console.WriteLine("---------------------------");
+            IrA.DentroDeRangoFunc(new Rango(30, 80), nNumeros).ForEach(Console.WriteLine);
+            Console.WriteLine("---------------------------");
+            IrA.DentroDeRangoFuncTuple(Tuple.Create(30, 80), nNumeros).ForEach(Console.WriteLine);
+        }
+
+        List<int> DentroDeRango(Rango oRango, List<int> nNumeros) {
+            List<int> nNumerosDentro = new List<int>();
+
+            foreach (int nNumero in nNumeros) {
+                if (nNumero >= oRango.Inferior && nNumero <= oRango.Superior)
+                    nNumerosDentro.Add(nNumero);
+            }
+            return nNumerosDentro;
+        }
+
+        Func<Rango, List<int>, List<int>> DentroDeRangoFunc = (a, b)
+             => b.FindAll(x => x >= a.Inferior && x <= a.Superior);
+
+        Func<Tuple<int, int>, List<int>, List<int>> DentroDeRangoFuncTuple = (a, b)
+               => b.FindAll(x => x >= a.Item1 && x <= a.Item2);
+    }
+}
---
+++ Rango.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiparadigma {
+    class Rango {
+        private int nInferior;
+
+        private int nSuperior;
+
+        public int Inferior {
+            get{ return nInferior; }
+            set { nInferior = value; }
+        }
+
+        public int Superior {
+            get { return nSuperior; }
+            set { nSuperior = value; }
+        }
+
+        public Rango(int nInferior, int nSuperior) {
+            this.nInferior = nInferior;
+            this.nSuperior = nSuperior;
+        }
+    }
+}
